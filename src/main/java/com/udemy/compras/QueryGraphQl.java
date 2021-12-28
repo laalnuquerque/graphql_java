bb24c4a765ba93dfd20f67a550affe5eaa5125fa
Larissa Miranda <larissamiranda@MacBook-Air-de-Larissa.local>
2021-12-28 19:18:37 -0300

    configuração de banco de dados

--- src/main/java/com/udemy/compras/QueryGraphQl.java
+++ src/main/java/com/udemy/compras/QueryGraphQl.java
@@ -1,12 +1,32 @@
 package com.udemy.compras;
 
-import com.coxautodev.graphql.tools.GraphQLResolver;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
-public class QueryGraphQl implements GraphQLResolver {
+public class QueryGraphQl implements GraphQLQueryResolver {
+
+    @Autowired
+    private ClienteRepository clienteRep;
     public String hello(){
         return "Hello filho";
     }
 
+    public int soma(int a, int b){
+        return a+b;
+    }
+
+    public Cliente cliente(Long id){
+
+        return clienteRep.findById(id).orElse(null);
+    }
+
+    public List<Cliente> clientes(){
+        return clienteRep.findAll();
+    }
 }
